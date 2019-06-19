2ad9df19bb9297a4b33550f1d5e83970efc115a3
GBTC441012UR <GBTC441012UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-19 14:47:36 -0400

    first commit

---
+++ src/ArrayListDebugging.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+public class ArrayListDebugging {
+    public static void main(String [] args){
+                // Create an arraylist
+                ArrayList <String> alArrayList = new ArrayList<>();// change the data type and the name to string and alArrayList
+                System.out.println("Initial size of alArrayList: " + alArrayList.size());
+
+                // Add elements to the arraylist
+        alArrayList.add("C");//instead of al change the name of the array to alArrayList
+        alArrayList.add("A");
+        alArrayList.add("E");
+        alArrayList.add("B");
+        alArrayList.add("D");
+        alArrayList.add("F");
+        alArrayList.add(1, "A2");
+                System.out.println("Size of al after additions: " + alArrayList.size());// change sizes to size
+
+                // Display the arraylist
+                System.out.println("Contents of alArrayList: " + alArrayList);
+
+                // Removes elements from the arraylist
+        alArrayList.remove("F");
+        alArrayList.remove("G");
+        alArrayList.remove(2);
+
+                System.out.println("Size of al after deletions: " +alArrayList.size());//change sizes to size
+                System.out.println("Contents of alArrayList: " +alArrayList);
+            }
+        }
+
+
